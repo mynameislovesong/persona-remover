f463d052ca98f2478fe8e4a3f0c9ca289df5fa47
mynameislovesong <hihit0072@gmail.com>
2026-08-31 16:25:44 +0900

    Load floating search status styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './landing-title.css'
+import './search-status.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
